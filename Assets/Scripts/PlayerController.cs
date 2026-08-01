a3fc453e86e61dd487e54d15b29ec7a526e70112
VicLatino7 <viclatino7@gmail.com>
2026-08-01 03:23:05 -0500

    Skybox and clamping
    Imported skybox and implemented clamping to player movement on x and z axis

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,6 +4,8 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] float moveSpeed = 5f;
+    [SerializeField] float xClamp = 3f;
+    [SerializeField] float zClamp = 2f;
     Vector2 movement;
 
     Rigidbody rigidBody;
@@ -27,6 +29,9 @@ public class PlayerController : MonoBehaviour
         Vector3 currentPosition = rigidBody.position;
         Vector3 moveDirection = new Vector3(movement.x, 0f, movement.y);
         Vector3 newPosition = currentPosition + moveDirection * (Time.fixedDeltaTime * moveSpeed);
+        
+        newPosition.x = Mathf.Clamp(newPosition.x, -xClamp, xClamp);
+        newPosition.z = Mathf.Clamp(newPosition.z, -zClamp, zClamp);
         rigidBody.MovePosition(newPosition);
     }
 }
