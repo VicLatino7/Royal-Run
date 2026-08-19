cd58d0dc498a445fd585608dacf4f68214a476e2
VicLatino7 <viclatino7@gmail.com>
2026-08-19 01:18:54 -0500

    Implementing apple and coin pickups
    created both apple and coin prefabs/pickups and introduced spawning for apples. Have yet to implement coins spawn logic

--- Assets/Scripts/Chunk.cs
+++ Assets/Scripts/Chunk.cs
@@ -4,28 +4,49 @@ using System.Collections.Generic;
 public class Chunk : MonoBehaviour
 {
     [SerializeField] GameObject fencePrefab;
+    [SerializeField] GameObject applePrefab;
 
-    [SerializeField] float[] lanes = {-4.15f, -1.21f,1.71f};
+    [SerializeField] float appleSpawnChance = 0.3f;
 
+    [SerializeField] float[] lanes = {-4.15f, -1.21f, 1.71f};
+
+    List<int> availableLanes = new List<int> { 0, 1, 2 };
     void Start()
     {
-        SpawnFence();
+        SpawnFences();
+        SpawnApple();
     }
 
-    void SpawnFence()
+    void SpawnFences()
     {
-        List<int> availableLanes = new List<int> { 0, 1, 2 };
-        int fencesToSpawn = Random.Range(0, lanes.Length);
+        int fencesToSpawn = Random.Range(0, lanes.Length); // Randomly determine how many fences to spawn (0 to 2)
 
-        for (int i = 0; i < fencesToSpawn; i++)
+        for (int i = 0; i < fencesToSpawn; i++) // Loop to spawn fences
         {
             if(availableLanes.Count <= 0) break;
             
-            int randomLaneIndex = Random.Range(0, availableLanes.Count);
-            int selectedLane = availableLanes[randomLaneIndex];
-            availableLanes.RemoveAt(randomLaneIndex);
+            int selectedLane = SelectLane(); // Select a lane for the fence
+
             Vector3 spawnPosition = new Vector3(lanes[selectedLane], transform.position.y, transform.position.z);
             Instantiate(fencePrefab, spawnPosition, Quaternion.identity, this.transform);
         }
+    }
+
+    void SpawnApple()
+    {
+        if (Random.value > appleSpawnChance || availableLanes.Count <= 0) return; // Check if we should spawn an apple based on the spawn chance
+        if(availableLanes.Count <= 0) return;
+
+        int selectedLane = SelectLane(); // Select a lane for the apple
+
+        Vector3 spawnPosition = new Vector3(lanes[selectedLane], transform.position.y, transform.position.z);
+        Instantiate(applePrefab, spawnPosition, Quaternion.identity, this.transform);   
+    }
+    int SelectLane()
+    {
+        int randomLaneIndex = Random.Range(0, availableLanes.Count);
+        int selectedLane = availableLanes[randomLaneIndex];
+        availableLanes.RemoveAt(randomLaneIndex);
+        return selectedLane;
     }
 }
